d88b8123faea4958e63aae5245c7afe79c060b3f
Snigdha17 <snigdhu17@gmail.com>
2021-09-26 18:08:10 -0700

    feat: Added emojipedia source code

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "🙂": "smiling",
+  "😏": "smirking",
+  "🤥": "lying",
+  "🤧": "sneezing",
+  "😟": "worried",
+  "🥵": "hot face",
+  "🥶": "cold face",
+  "🤯": "exploding head",
+  "😲": "astonished face"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function onChangeEventHandler(event) {
+    var userInput = event.target.value;
+
+    if (userInput in emojiDictionary) {
+      setMeaning(emojiDictionary[userInput]);
+    } else {
+      setMeaning("Unknown emoji");
+    }
+  }
+
+  function emojiClickHandler(emoji) {
+    setMeaning(emojiDictionary[emoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji - pretor</h1>
+      <h3>Enter an emoji below to find its meaning</h3>
+      <input onChange={onChangeEventHandler}></input>
+      <h2 id="meaning">{meaning} </h2>
+      <h3>Or try one of the emojis below!</h3>
+      <div id="emojiDisplay">
+        {emojisWeKnow.map((emoji) => {
+          return (
+            <span
+              onClick={() => emojiClickHandler(emoji)}
+              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            >
+              {emoji}{" "}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
